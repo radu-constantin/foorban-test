594cc5c4affb8479cbe3f8afcd7e72779972f175
Radu Constantin <raduadrian.constantin94@gmail.com>
2024-05-11 14:03:41 +0300

    Added 2 e2e tests

--- backend/src/__tests__/app.e2e-spec.ts
+++ backend/src/__tests__/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../app.module';
 
-describe('AppController (e2e)', () => {
+describe('/info/validateUser POST', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,8 +15,35 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/info/validateUser (POST)', () => {
-    const response = request(app.getHttpServer()).post('/info/validateUser');
-    response.expect(400);
+  test('Empty request body results in 400 status code', () => {
+    return request(app.getHttpServer()).post('/info/validateUser').expect(400);
+  });
+
+  test('Correct input results in 200 status code', () => {
+    return request(app.getHttpServer())
+      .post('/info/validateUser')
+      .send({ name: 'radu c', age: 30, married: true, dob: '1994-03-14' })
+      .expect(201)
+      .expect(({ body }) => {
+        expect(body.success).toBe(true);
+      });
+  });
+
+  test('Only name input results in error messages for missing age and dob', () => {
+    return request(app.getHttpServer())
+      .post('/info/validateUser')
+      .send({ name: 'radu c' })
+      .expect(400)
+      .expect(({ body }) => {
+        expect(body).toHaveLength(2);
+        expect(body[0].constraints).toHaveProperty(
+          'isNumber',
+          'age must be a number conforming to the specified constraints',
+        );
+        expect(body[1].constraints).toHaveProperty(
+          'isNotEmpty',
+          'dob should not be empty',
+        );
+      });
   });
 });
